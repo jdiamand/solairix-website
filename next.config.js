f043b7fc50576be0b538dc2f3cba3658656e07d4
Joe Diamand <jdiamand@yahoo.com>
2025-02-06 00:04:07 -0500

    Fix AWS Amplify deployment with correct artifact directory

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // Enable static export mode
+  distDir: "out", // Optional: Change output directory to "out"
+};
+
+module.exports = nextConfig;
