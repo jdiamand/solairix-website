31df89a05b3c0c2a4f7e989208b2c5f2bb150d24
Joe Diamand <jdiamand@yahoo.com>
2025-02-06 00:15:08 -0500

    Fix Next.js App Router deployment on AWS Amplify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Enable static export mode
   distDir: "out", // Optional: Change output directory to "out"
 };
 
