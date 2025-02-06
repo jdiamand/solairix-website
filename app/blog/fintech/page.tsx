75058bc5a67482e55e668441beeb2b6e27260ac5
Joe Diamand <jdiamand@yahoo.com>
2025-02-06 03:20:13 -0500

    Fix FinTech blog page export issue

---
+++ app/blog/fintech/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogCategoryFinTech() {
+    return (
+        <div>
+            <h1>FinTech Blog Posts</h1>
+            <p>Welcome to the FinTech category of the Solairix Blog.</p>
+        </div>
+    );
+}
